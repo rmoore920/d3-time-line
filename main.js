618e9907d74f8eb4d3a876a00139df0ea214e1f1
R. Moore <rmoore920@users.noreply.gihub.com>
2021-07-14 15:16:17 -0700

    Initial checkin

---
+++ main.js
@@ -0,0 +1,12 @@
+import * as d3 from "https://unpkg.com/d3?module"
+
+const dateParser = d3.timeParse("%Y-%m-%d")
+const dates = d => dateParser(d.date)
+
+async function draw(){
+  const data = await d3.json("./data/data.json")
+  const x = dates(data[0])
+  console.log(x)
+}
+
+draw()
