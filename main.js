9a57c8b28428631ede58c65933e8c78aeb6864a1
R. Moore <rmoore920@users.noreply.gihub.com>
2021-07-21 10:44:39 -0700

    Updated

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import * as d3 from "https://unpkg.com/d3?module"
 
 const dateParser = d3.timeParse("%Y-%m-%d")
-const dates = d => dateParser(d.date)
+const dates = (d) => dateParser(d.date)
 
-async function draw(){
+async function draw() {
   const data = await d3.json("./data/data.json")
   const x = dates(data[0])
   console.log(x)
+  const y = (d) => d.temperatureMax
+  console.log(y)
 }
 
 draw()
